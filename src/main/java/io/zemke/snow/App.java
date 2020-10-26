be6369ce42cf5488fbfbacb43191209a10beda1c
Zemke <florian@zemke.io>
2020-10-26 13:29:00 +0100

    All done

--- src/main/java/io/zemke/snow/App.java
+++ src/main/java/io/zemke/snow/App.java
@@ -1,5 +1,6 @@
 package io.zemke.snow;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -11,28 +12,46 @@ import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class App extends Application {
+
+    public static final int PADDING = 90;
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 728;
+
+    private static final int INTERVAL = 20;
 
     private double xOffset = 0;
     private double yOffset = 0;
 
+    private final List<Flake> snow = new ArrayList<>();
+    private final Pane container = new Pane();
+
+    private List<Flake> createSnow(int amount) {
+        var snow = new ArrayList<Flake>();
+        for (var i = 0; i < amount; i++) {
+            snow.add(new Flake());
+        }
+        return snow;
+    }
+
     @Override
     public void start(Stage stage) {
         stage.setTitle("Snow");
-        var javaVersion = System.getProperty("java.version");
-        var javafxVersion = System.getProperty("javafx.version");
-        System.out.println("javaVersion " + javaVersion);
-        System.out.println("javafxVersion " + javafxVersion);
-        var container = new StackPane();
-        var width = 1000;
-        var height = 728;
-        var backgroundImage = new BackgroundImage(new
-                Image(App.class.getResourceAsStream("/pic.png"), width, height, false, true),
+        System.out.println("javaVersion " + System.getProperty("java.version"));
+        System.out.println("javafxVersion " + System.getProperty("javafx.version"));
+        addFlakes(createSnow(10));
+        var backgroundImage = new BackgroundImage(
+                new Image(App.class.getResourceAsStream("/pic.png"), WIDTH, HEIGHT, false, true),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
                 BackgroundSize.DEFAULT);
         container.setBackground(new Background(backgroundImage));
@@ -44,16 +63,46 @@ public class App extends Application {
             stage.setX(event.getScreenX() - xOffset);
             stage.setY(event.getScreenY() - yOffset);
         });
-        container.setPadding(new Insets(90, 90, 90, 90));
-        var scene = new Scene(container, width, height);
+        container.setPadding(new Insets(PADDING));
+        var scene = new Scene(container, WIDTH, HEIGHT);
         scene.getRoot().setEffect(new DropShadow(BlurType.GAUSSIAN, Color.BLACK, 45, 0, 0, 10));
         scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
-        stage.setWidth(width + 90);
-        stage.setHeight(height + 90);
+        stage.setWidth(WIDTH + PADDING);
+        stage.setHeight(HEIGHT + PADDING);
         stage.setResizable(false);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
+        new AnimationTimer() {
+            @Override
+            public void handle(long currentNanoTime) {
+                try {
+                    Thread.sleep(INTERVAL);
+                    loop();
+                } catch (InterruptedException ignored) {
+                }
+            }
+        }.start();
+    }
+
+    private void loop() {
+        removeFlakes();
+        addFlakes(createSnow(random(4)));
+    }
+
+    private void addFlakes(List<Flake> flakes) {
+        snow.addAll(flakes);
+        container.getChildren().addAll(
+                flakes.stream().map(Flake::getCircle).toArray(Circle[]::new));
+    }
+
+    private void removeFlakes() {
+        snow.removeIf(flake -> flake.fall() > (HEIGHT + PADDING / 2 - 10));
+        container.getChildren().removeIf(child -> ((Circle) child).getCenterY() > (HEIGHT + PADDING / 2 - 10));
+    }
+
+    public static int random(int max) {
+        return new Random().nextInt(max);
     }
 
     public static void main(String[] args) {
